58bf2e74804cee335ee19bb43f5406d51c402708
andma-dotcom <andreamorganti2317@gmail.com>
2026-03-25 23:19:04 +0100

    yep

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,10 @@
-@import 'tailwindcss' source(none);
-@source '../**/*.{js,ts,jsx,tsx}';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-@import 'tw-animate-css';
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
